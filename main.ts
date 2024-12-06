c447ce96fdef8a716827e2feb26f0b4f85778e6e
hesu <hesu@zhaw.ch>
2024-12-06 18:53:10 +0100

    update main

--- main.ts
+++ main.ts
@@ -2,20 +2,21 @@ NeoPixelMatrix.sliderValueChangedThread(function () {
     basic.showNumber(NeoPixelMatrix.readSlider())
     serial.writeValue("slider", NeoPixelMatrix.readSlider())
     NeoPixelMatrix.setCurrentTime(h, m, s)
-    serial.writeValue("jostick", NeoPixelMatrix.readJoystick())
-    serial.writeString(NeoPixelMatrix.readJoystickText())
-})
-NeoPixelMatrix.joystickDirectionThread("up", function () {
-    NeoPixelMatrix.scrollText("up", 0xff0000, 91)
 })
 let s = 0
 let m = 0
 let h = 0
 NeoPixelMatrix.debugEnable(true)
 NeoPixelMatrix.initializeMatrix(DigitalPin.P0, 135)
+NeoPixelMatrix.createWordClock(
+1,
+0x00ff00,
+0x007fff,
+0xff0000
+)
 h = 0
-m = 0
-s = 0
+m = 22
+s = 55
 while (true) {
     basic.pause(5000)
     serial.writeLine(NeoPixelMatrix.getCurrentTimeAsText())
